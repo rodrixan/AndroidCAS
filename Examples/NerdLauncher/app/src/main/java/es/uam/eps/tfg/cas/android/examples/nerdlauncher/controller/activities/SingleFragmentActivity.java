f5341f61058d6b31fda27d0a3bee4787975ba158
Rodri de Blas <rodri.xan@gmail.com>
2016-04-05 16:03:53 +0200

    Minimal changes

--- Examples/NerdLauncher/app/src/main/java/es/uam/eps/tfg/cas/android/examples/nerdlauncher/controller/activities/SingleFragmentActivity.java
+++ Examples/NerdLauncher/app/src/main/java/es/uam/eps/tfg/cas/android/examples/nerdlauncher/controller/activities/SingleFragmentActivity.java
@@ -3,12 +3,12 @@ package es.uam.eps.tfg.cas.android.examples.nerdlauncher.controller.activities;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import es.uam.eps.tfg.cas.android.examples.nerdlauncher.R;
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
     private FragmentManager mFragmentManager;
