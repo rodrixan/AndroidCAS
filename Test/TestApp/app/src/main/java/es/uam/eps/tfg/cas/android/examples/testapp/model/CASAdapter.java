e660c0db134c552ce6e5e342ab589894000b8355
Rodri de Blas <rodri.xan@gmail.com>
2016-04-17 15:39:14 +0200

    Thinking how to draw expressions

---
+++ Test/TestApp/app/src/main/java/es/uam/eps/tfg/cas/android/examples/testapp/model/CASAdapter.java
@@ -0,0 +1,5 @@
+package es.uam.eps.tfg.cas.android.examples.testapp.model;
+
+
+public interface CASAdapter {
+}
---
+++ Test/TestApp/app/src/main/java/es/uam/eps/tfg/cas/android/examples/testapp/view/ExpressionView.java
@@ -0,0 +1,22 @@
+package es.uam.eps.tfg.cas.android.examples.testapp.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ExpressionView extends View {
+
+    public ExpressionView(final Context context) {
+        this(context, null);
+    }
+
+    public ExpressionView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
